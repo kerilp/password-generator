b42644f99e646fc4d11a463461b3548f4493a303
Keri Price <keri.lynne.p@gmail.com>
2021-08-20 21:30:38 -0400

    updated formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,47 +8,47 @@ var eachChoice = [];
 var charTypes = "";
 
 function generatePassword() {
-  
+
   var password = "";
   var hasLowerCase = confirm("Do you want to include lowercase characters?");
   var hasUpperCase = confirm("Do you want to include uppercase characters?");
   var hasNumbers = confirm("Do you want to include number characters?");
   var hasSpecial = confirm("Do you want to include special characters?");
 
-  if(hasLowerCase) {
+  if (hasLowerCase) {
     var random = Math.floor(Math.random() * lowercase.length);
     var choice = lowercase[random];
     eachChoice.push(choice);
     charTypes = charTypes.concat(lowercase);
   }
-  if(hasUpperCase) {
+  if (hasUpperCase) {
     var random = Math.floor(Math.random() * uppercase.length);
     var choice = uppercase[random];
     eachChoice.push(choice);
     charTypes = charTypes.concat(uppercase);
   }
-  if(hasNumbers) {
+  if (hasNumbers) {
     var random = Math.floor(Math.random() * numbers.length);
     var choice = numbers[random];
     eachChoice.push(choice);
     charTypes = charTypes.concat(numbers);
   }
-  if(hasSpecial) {
+  if (hasSpecial) {
     var random = Math.floor(Math.random() * special.length);
     var choice = special[random];
     eachChoice.push(choice);
     charTypes = charTypes.concat(special);
   }
-  if(!hasLowerCase && !hasUpperCase &&!hasNumbers && !hasSpecial) {
+  if (!hasLowerCase && !hasUpperCase && !hasNumbers && !hasSpecial) {
     alert("You must select at least one character type.");
     return;
   }
 
   var length = prompt("Choose a length of at least 8 characters and no more than 128 characters.");
 
-  if(length > 7 && length < 129) {
-    
-    for(var i = 0; i < length - eachChoice.length; i++){
+  if (length > 7 && length < 129) {
+
+    for (var i = 0; i < length - eachChoice.length; i++) {
       var random = Math.floor(Math.random() * charTypes.length);
       password = password + charTypes[random];
     }
@@ -58,7 +58,7 @@ function generatePassword() {
     alert("Invalid input. Please try again.");
     return;
   }
-  
+
   charTypes = "";
   return password;
 }
